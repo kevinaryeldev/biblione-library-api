993f7f9727e3158b1b4af536ecc0b13a4f1a7108
Kevin Aryel <kevinaryel.dev@gmail.com>
2026-03-13 14:36:20 -0300

    chore: configuracao inicial do Spring

---
+++ src/main/java/com/biblione/library_api/BiblioneLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.biblione.library_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioneLibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioneLibraryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biblione/library_api/BiblioneLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblione.library_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioneLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
